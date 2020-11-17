0b316b8fafbf40e382e37ebc58b67966fbbf672b
jieniu$ <jienius@outlook.com>
2020-11-16 22:23:16 -0700

    Card

--- atm/src/components/CardScannerComponent.jsx
+++ atm/src/components/CardScannerComponent.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CardScannerComponent.css"
 
 const CardScanner = (props) => {
+    const [inserted, setInserted] = useState(props.cardScanner.inserted);    
+
     const onClick = () => {
         if (!props.cardScanner.inserted) {
             props.cardScanner.inserted = true;
             props.cardScanner.accountNumber = "0123-4567-8987-6543";
             props.cardScanner.read = true;
-        }
+        } 
+        setInserted(props.cardScanner.inserted);
     }
     
     return (<div className="CardScanner">
-        <button className="Card" onClick={onClick}></button>
+        <button className="CardSlot" onClick={onClick}></button>
+        <div className={props.cardScanner.inserted ? "Card" : "Card-invisible"} />
     </div>)
 }
 
